6e830d898d8537833d4f167df3cbdc608959b697
Myrhoiazov <denis.mirgoyazov@gmail.com>
2022-08-10 00:50:14 +0300

    add 2 task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,19 @@
+import Player from '@vimeo/player';
+
+import throttle from 'lodash.throttle';
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const onPlay = function (data) {
+  localStorage.setItem('videoplayer-current-time', data.seconds);
+};
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+const currentTime = localStorage.getItem('videoplayer-current-time') ? localStorage.getItem('videoplayer-current-time') : 0;
+
+player.setCurrentTime(currentTime);
+
+
 
